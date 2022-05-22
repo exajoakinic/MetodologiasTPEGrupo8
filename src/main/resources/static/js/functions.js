c256ed3a550257a538dda69702f9ee5e424900ed
Joaquin Iglesias Coqueti <jiglesiascoqueti@alumnos.exa.unicen.edu.ar>
2022-05-21 22:40:02 -0300

    TF-59 <Se terminó de corregir funcion turnosDisponiblesEntreFechas(from, to) del FrontEnd que se comunica con el BackEnd para traer los turnos disponibles>

--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,49 +1,12 @@
-
-function test() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var raw = JSON.stringify({
-     "from": "2022-05-13",
-     "to": "2022-05-13"
-    });
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        data: raw,
-        redirect: 'follow'
-    };
-
-    fetch("localhost:8080/turno/entrefechas", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-}
-
-async function listarTurnosDisponiblesEntreFechas(fechaInicio, fechaFin) {
- try {
-    let url = '/turno/entrefechas';
-    let raw = JSON.stringify({"from": fechaInicio, "to": fechaFin})
-    console.log(raw)
-    let obj = await fetch(url,{
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          method: "GET",
-          body: raw
-      }
-    );
-    let data = await obj.json();
-    let turnosDisp = [];
-    let domTurnos = document.querySelector("#divTurnosDisponibles");
-    domTurnos.innerHTML = "";
-    for (let i = 0; i < data.length; i++) {
-        domTurnos.innerHTML += data[i];
+async function turnosDisponiblesEntreFechas(from, to) {
+    try {
+        let obj = await fetch('/turno/turnosdisponibles/' + from + '/' + to);
+        let data = await obj.json();
+        //console.log(data)
+        return data;
     }
- }
- catch (error) {
-    console.log(error)
+    catch (error) {
+       console.log(error)
+    }
 }
 }
